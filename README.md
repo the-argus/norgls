9d4dbd250f873fd022b54e132b4cd060c9aea5dc
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-11 20:50:52 -0600

    init async

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # norgls
 
 a LSP implementation for neorgs .norg markdown-like filetype
+
+## intended features
+
+check out the [discussion](https://github.com/nvim-neorg/neorg/issues/513)
--- src/norgls.nim
+++ src/norgls.nim
@@ -1,1 +1,16 @@
-echo("hello")
+import std/[asyncdispatch, asyncfile, streams, strutils]
+import asynctools/asyncproc
+
+proc main(ins: Stream | AsyncFile, outs: Stream | AsyncFile) {.multisync.} =
+    echo("hello")
+
+when defined(windows):
+  var
+    ins = newFileStream(stdin)
+    outs = newFileStream(stdout)
+  main(ins, outs)
+else:
+  var
+    ins = newAsyncFile(stdin.getOsFileHandle().AsyncFD)
+    outs = newAsyncFile(stdout.getOsFileHandle().AsyncFD)
+  waitFor main(ins, outs)
