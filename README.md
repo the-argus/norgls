b29eae721bb4eb7b14a913eaff3a8170069232f6
Ian McFarlane <i.mcfarlane2002@gmail.com>
2023-01-12 19:51:52 -0600

    add basic type definitions from nimlsp and async i/o

--- README.md
+++ README.md
@@ -5,3 +5,8 @@ a LSP implementation for neorgs .norg markdown-like filetype
 ## intended features
 
 check out the [discussion](https://github.com/nvim-neorg/neorg/issues/513)
+
+## credit
+
+huge thanks to pmunch for creating [nimlsp](https://github.com/PMunch/nimlsp),
+from which I copied just about everything in this repository.
--- src/norgls.nim
+++ src/norgls.nim
@@ -1,16 +1,49 @@
-import std/[asyncdispatch, asyncfile, streams, strutils]
+import json
+import std/[asyncdispatch, asyncfile, streams, uri]
 import asynctools/asyncproc
 
-proc main(ins: Stream | AsyncFile, outs: Stream | AsyncFile) {.multisync.} =
-    echo("hello")
+include lsp/messages
 
-when defined(windows):
-  var
-    ins = newFileStream(stdin)
-    outs = newFileStream(stdout)
-  main(ins, outs)
-else:
-  var
-    ins = newAsyncFile(stdin.getOsFileHandle().AsyncFD)
-    outs = newAsyncFile(stdout.getOsFileHandle().AsyncFD)
-  waitFor main(ins, outs)
+type
+  LogLevel {.pure.} = enum
+    debug, warn, error
+
+proc log(level: LogLevel, args: varargs[string, `$`]) =
+  if level == LogLevel.debug:
+    for arg in args:
+      echo(arg)
+  elif level == LogLevel.warn:
+    for arg in args:
+      echo(arg)
+  elif level == LogLevel.error:
+    for arg in args:
+      echo(arg)
+
+template whenValid(data, kind, body) =
+  if data.isValid(kind):
+    var data = kind(data)
+    body
+  else:
+    debug.log("Unable to parse data as ", kind)
+
+proc main(ins: Stream | AsyncFile, outs: Stream | AsyncFile) {.async.}=
+  while true:
+    try:
+      let frame = await ins.readLine
+      let message = frame.parseJson
+      whenValid(message, RequestMessage):
+        echo("recieved valid LSP request")
+    except UriParseError as e:
+      LogLevel.warn.log("Got exception parsing URI: ", e.msg)
+      continue
+    except IOError as e:
+      LogLevel.error.log("Got IOError: ", e.msg)
+      break
+    except CatchableError as e:
+      LogLevel.warn.log("Got exception: ", e.msg)
+      continue
+
+var
+  ins = newAsyncFile(stdin.getOsFileHandle().AsyncFD)
+  outs = newAsyncFile(stdout.getOsFileHandle().AsyncFD)
+waitFor main(ins, outs)
